aaa3e3d119ee428cf832e86589c9f6798b95dfae
take-my-evolution <kkpsykk@gmail.com>
2023-05-19 19:08:43 +0300

    добавлен intel-ucode, переработана структура скрипта

--- arch_install.sh
+++ arch_install.sh
@@ -13,9 +13,7 @@ echo -n "Enter hostname "
 read hostname
 
 #очитска mnt
-umount -l /mnt/*
 umount -l /mnt
-umount -l /mnt/boot/efi
 rm -rf /mnt/*
 #Создание разделов sdaumount -l /mnt
 (echo o; sleep 1; echo  w) | fdisk /dev/sda
@@ -43,41 +41,59 @@ mount /dev/sda1 /mnt/boot/efi
 sed -i s/'#ParallelDownloads = 5'/'ParallelDownloads = 13'/g /etc/pacman.conf
 reflector --verbose -l 5 -p https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syy
-pacstrap -i /mnt base base-devel  linux  linux-firmware reflector --noconfirm 
+pacstrap -i /mnt base base-devel  linux  linux-firmware reflector intel-ucode --noconfirm 
 genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt /bin/bash -c "echo 'export username=$username' >> /etc/bash.bashrc"
+
+arch-chroot /mnt /bin/bash -c "sed -i s/'# %wheel ALL=(ALL:ALL) ALL'/'%wheel ALL=(ALL:ALL) ALL'/g /etc/sudoers"
+arch-chroot /mnt /bin/bash -c "echo '%wheel ALL=(ALL) NOPASSWD: /usr/bin/pacman'>>/etc/sudoers"
+
 arch-chroot /mnt /bin/bash -c "sed -i s/'#Color'/'Color'/g /etc/pacman.conf"
-arch-chroot /mnt /bin/bash -c "ln -fs /usr/share/zoneinfo/Europe/Moscow /etc/localtime"
-arch-chroot /mnt /bin/bash -c "timedatectl set-ntp true" 
-arch-chroot /mnt /bin/bash -c "hwclock --systohc"
 arch-chroot /mnt /bin/bash -c "sed -i s/'#en_US.UTF-8'/'en_US.UTF-8'/g /etc/locale.gen"
 arch-chroot /mnt /bin/bash -c "sed -i s/'#ru_RU.UTF-8'/'ru_RU.UTF-8'/g /etc/locale.gen"
+arch-chroot /mnt /bin/bash -c "sed -i s/'#ParallelDownloads = 5'/'ParallelDownloads = 13'/g /etc/pacman.conf"
+arch-chroot /mnt /bin/bash -c "sed -i '/^BINARIES=/ s/)/ $binmod)/' /etc/mkinitcpio.conf"
+
 arch-chroot /mnt /bin/bash -c "locale-gen"
 arch-chroot /mnt /bin/bash -c "echo 'LANG=en_US.UTF-8' > /etc/locale.conf"
 arch-chroot /mnt /bin/bash -c "echo 'KEYMAP=ru' > /etc/vconsole.conf"
 arch-chroot /mnt /bin/bash -c "echo 'FONT=cyr-sun16' >> /etc/vconsole.conf"
+
 arch-chroot /mnt /bin/bash -c "echo '$hostname' > /etc/hostname"
 arch-chroot /mnt /bin/bash -c "echo '127.0.0.1 localhost' > /etc/hosts"
 arch-chroot /mnt /bin/bash -c "echo '::1       localhost' >> /etc/hosts"
 arch-chroot /mnt /bin/bash -c "echo '127.0.0.1 $hostname.localdomain $hostname' >> /etc/hosts"
-arch-chroot /mnt /bin/bash -c "sed -i s/'#ParallelDownloads = 5'/'ParallelDownloads = 13'/g /etc/pacman.conf"
+
+arch-chroot /mnt /bin/bash -c "ln -fs /usr/share/zoneinfo/Europe/Moscow /etc/localtime"
+arch-chroot /mnt /bin/bash -c "timedatectl set-ntp true" 
+arch-chroot /mnt /bin/bash -c "hwclock --systohc"
+
 arch-chroot /mnt /bin/bash -c "reflector --verbose -l 5 -p https --sort rate --save /etc/pacman.d/mirrorlist"
-arch-chroot /mnt /bin/bash -c "sed -i '/^BINARIES=/ s/)/ $binmod)/' /etc/mkinitcpio.conf"
+
 arch-chroot /mnt /bin/bash -c "mkinitcpio -p linux"
+arch-chroot /mnt /bin/bash -c "pacman -Syu"
 arch-chroot /mnt /bin/bash -c "pacman -S $pacman_list --noconfirm"
+
 arch-chroot /mnt /bin/bash -c "systemctl enable NetworkManager"
 arch-chroot /mnt /bin/bash -c "systemctl enable sshd.service"
 arch-chroot /mnt /bin/bash -c "grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id='Arch'"
 arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
+
 arch-chroot /mnt /bin/bash -c "echo 'root:$root_password' | chpasswd" 
 arch-chroot /mnt /bin/bash -c "useradd -m -G wheel -s /bin/bash $username"
 arch-chroot /mnt /bin/bash -c "echo '$username:$user_password' | chpasswd" 
-arch-chroot /mnt /bin/bash -c "sed -i s/'# %wheel ALL=(ALL:ALL) ALL'/'%wheel ALL=(ALL:ALL) ALL'/g /etc/sudoers"
+
+export username=$username
+mkdir "/mnt/home/$username/sh/"
+cp "$BASEDIR/yay_install.sh" "/mnt/home/$username/sh/yay_install.sh" 
+arch-chroot /mnt /bin/bash -c "chmod -R +x '/home/$username/sh'"
 
 #instal yay
-sed -i "s/username/$username/g" yay_install.sh
-echo '%wheel ALL=(ALL) NOPASSWD: /usr/bin/pacman'>>/mnt/etc/sudoers
-cp $BASEDIR/yay_install.sh /mnt/home/$username/yay_install.sh 
-arch-chroot /mnt /bin/bash -c "chmod +x /home/$username/yay_install.sh"
-arch-chroot /mnt /bin/bash -c "sh /home/$username/yay_install.sh"
+arch-chroot /mnt /bin/bash -c "sh /home/$username/sh/yay_install.sh"
+sed -i s/'export username=$username'/''/g /mnt/etc/bash.bashrc
 sed -i '$d' /mnt/etc/sudoers 
+rm -rf /mnt/home/$username/sh
 rm -rf /mnt/home/$username/yay
+
+
--- yay_install.sh
+++ yay_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo -u username mkdir /home/username/yay&&
-cd /home/username/yay&&
-sudo -u username curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'&&
-sudo -u username makepkg -si --noconfirm&&
+echo "u = $username"
+sudo -u $username mkdir "/home/$username/yay"&&
+cd "/home/$username/yay"&&
+sudo -u $username curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'&&
+sudo -u $username makepkg -si --noconfirm&&
 yay --version
